978518c391d95a3f7efb3c976d104f7ee8ec5b0e
HaoLovesCoding <nanocmp@gmail.com>
2017-03-01 21:24:57 -0800

    OOP Design and SYNACK FIN FINACK

--- project2/CS118_project2/CS118_project2/Server.cpp
+++ project2/CS118_project2/CS118_project2/Server.cpp
@@ -20,49 +20,7 @@
 #include <pthread.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-#include "Packet.h"
-int startUDP(int port){// it will start, and bind to a port, then start listening, then return the fd of
-    struct sockaddr_in my_address;
-    int sockid=socket(AF_INET, SOCK_DGRAM, 0);// return the file descriptor of the new socket
-    if (sockid==-1) {
-        perror("Socket Creation Failed");
-        exit(1);
-    }
-    memset((char*)&my_address, 0, sizeof(my_address));
-    my_address.sin_family=AF_INET;
-    my_address.sin_port=htons(port);
-    my_address.sin_addr.s_addr=htonl(INADDR_ANY);
-    if (bind(sockid, (struct sockaddr*) &my_address, sizeof(my_address))==-1) {// bind socket to add port
-        perror("Bind Failed");
-        exit(1);
-    }
-    return sockid;
-}
-
-int receiveSYN(int socket_fd){
-    ssize_t receive_len=-1;
-    char buffer[BUFFSIZE];
-    struct sockaddr_in client_address;
-    socklen_t clenet_addrlen=sizeof(client_address);
-    while (true) {
-        memset(buffer, 0, BUFFSIZE);
-        receive_len=recvfrom(socket_fd, buffer, BUFFSIZE, 0, (struct sockaddr*) &client_address, &clenet_addrlen);
-        if (receive_len<0) {
-            perror("Receive SYN error\n");
-            exit(1);
-        }
-        Data mydata(buffer,buffer+receive_len);
-        Packet recv_pkt(mydata);
-        if (recv_pkt.getSYN()) {
-            printf("SYN received!");
-            return recv_pkt.getSeq()+1;
-        }
-        else{
-            perror("SYN packet garbled\n");
-            continue;
-        }
-    }
-}
+#include "TCPServer.h"
 
 int main(int argc, const char * argv[]) {
     if (argc!=3) {
@@ -70,7 +28,10 @@ int main(int argc, const char * argv[]) {
         exit(1);
     }
     int port=atoi(argv[1]);
-    int socket_fd=startUDP(port);
-    receiveSYN(socket_fd);
+    TCPServer myserver(port);
+    myserver.receiveSYN();
+    myserver.sendSYNACK();
+    myserver.sendFIN();
+    myserver.receiveFINACK();
     return 0;
 }
